a5507c76828e785fbbb08e6193f515c9aa3589aa
Aryan <arrygoo@gmail.com>
2016-08-15 20:31:34 -0400

    Moved stuff to main.js

--- main.js
+++ main.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   // imageMapResizer
   $('map').imageMapResize();
 
+  function mouseOverEvent(area) {
+    $("#" + area).addClass("opaque");
+  }
+  function mouseOutEvent(area) {
+    $("#" + area).removeClass("opaque");
+  }
+  function openModal(modalName) {
+    $("#" + modalName).modal('show');
+  }
+
+s
 });
